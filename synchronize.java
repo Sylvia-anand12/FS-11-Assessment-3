c4b9a74af880e396ed7ec52c2fc58e0cdc9e35e7
Sylvia <103881850+Sylvia-anand12@users.noreply.github.com>
2024-03-18 15:45:52 +0530

    Update synchronize.java

--- synchronize.java
+++ synchronize.java
@@ -1,1 +1,47 @@
+class Counter {
+    private int cnt = 0;
 
+
+    public synchronized void increment() {
+        cnt++;
+    }
+
+
+    public synchronized int getCount() {
+        return cnt;
+    }
+}
+
+class MyThread extends Thread {
+    private Counter counter;
+
+    public MyThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        
+        Counter counter = new Counter();
+
+        MyThread thread1 = new MyThread(counter);
+        MyThread thread2 = new MyThread(counter);
+
+        thread1.start();
+        thread2.start();
+
+    
+        thread1.join();
+        thread2.join();
+
+       
+        System.out.println("Final Counter Value: " + counter.getCount());
+    }
+}
