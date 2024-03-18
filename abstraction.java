75470d1871f963dbde9aa23fdcbd4e5a27da415c
Sylvia <103881850+Sylvia-anand12@users.noreply.github.com>
2024-03-18 16:00:18 +0530

    Update abstraction.java

--- abstraction.java
+++ abstraction.java
@@ -1,39 +1,100 @@
-//Synchronization
-class Counter {
-   private int cnt = 0;
-   public synchronized void increment() {
-       cnt++;
-   }
-   public int getCount() {
-       return cnt;
-   }
+Abstraction in Java is a concept that focuses on hiding the implementation details of a class while exposing only the necessary features or behavior to the outside world. It allows us to create a simplified view of a complex system, making it easier to work with.
+
+There are two main ways to implement abstraction in Java:
+
+1. Abstract Classes
+2. Interfaces
+
+
+1. Abstract Classes:
+ 
+ abstract class Shape {
+        public abstract double calculateArea();
 }
 
-class IncrementerThread extends Thread {
-   private Counter counter;
-   private int times;
+class Circle extends Shape {
+    private double radius;
 
-   public IncrementerThread(Counter counter, int times) {
-       this.counter = counter;
-       this.times = times;
-   }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
-   public void run() {
-       for (int i = 0; i < times; i++) {
-           counter.increment();
-       }
-   }
+        @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
 }
 
-public class Main{
-   public static void main(String[] args) throws InterruptedException {
-       Counter counter = new Counter();
-       IncrementerThread thread1 = new IncrementerThread(counter, 1000);
-       IncrementerThread thread2 = new IncrementerThread(counter, 1000);
-       thread1.start();
-       thread2.start();
-       thread1.join();
-       thread2.join();
-       System.out.println("Final count: " + counter.getCount());
-   }
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+   
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+public class AbstractClassExample {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        System.out.println("Area of Circle: " + circle.calculateArea());
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+    }
+}
+
+
+
+2. Interfaces:
+
+
+    {
+    double calculateArea();
+}
+
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle implements Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+public class InterfaceExample {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        System.out.println("Area of Circle: " + circle.calculateArea());
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+    }
 }
